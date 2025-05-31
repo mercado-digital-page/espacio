0fb76340d9d49b46d7e2ab8ecd8b990afe7f78e1
Mercado Digital <mercadodigital963@gmail.com>
2025-05-30 20:05:46 -0500

    Add files via upload

--- brochure/template.js
+++ brochure/template.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const dataFile = '../../admin/clientes/data.json';
+    const dataFile = 'https://mercado-digital-page.github.io/admin/clientes/data.json';
     const companyId = window.companyProfileConfig?.companyId || ''; // Usa el ID configurado
 
-    // Resto del código permanece igual...
     function hexToRgb(hex) {
         const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
         hex = hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
